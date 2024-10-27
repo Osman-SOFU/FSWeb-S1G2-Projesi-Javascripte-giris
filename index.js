3c2a11eb4a37137a61c11f383760167568cb2953
Osman-SOFU <osofu1903@gmail.com>
2024-10-27 21:11:11 +0300

    Görevler Tamamlandı.

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ Aşağıdakileri yap:
 
    İPUCU: fonksiyon oluşturmaya gerek yok
 */
+const surucuYasi = 29;
+if(surucuYasi > 18){
+true
+}else {
+  false
+}
 
 /*
 Görev 1b - Değerler (puanlamaya dahil değildir)
@@ -31,6 +37,11 @@ Aşağıdakileri yap:
 
    İPUCU: fonksiyon oluşturmaya gerek yok
 */
+const birinciDeger = [0,1,2,3,4,5];
+const ikinciDegeri = [5,6,7,8,9,10];
+if( birinciDeger <= ikinciDegeri){
+  console.log(birinciDeger)
+}
 
 /*
 Görev 1c - String bir değeri Number'a dönüştürün (puanlamaya dahil değildir)
@@ -42,7 +53,9 @@ Aşağıdakileri yap:
 
    İPUCU: Number metoduna bakabilirsin
 */
-
+let yearString = "1999";
+let yearNumber = Number(yearString);
+console.log(yearNumber);
 /*
 Görev 1d - Çarpma
  
@@ -52,8 +65,8 @@ Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:
    3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function carpma(a, b) {
+return a* b
 }
 
 /* Görev 2 : Köpeğin Yaşı */
@@ -65,8 +78,8 @@ Aşağıdakileri yap:
  3. Hesaplanan köpeğin yaşını dönün.
  */
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function kopeginYasi(yas) {
+return yas * 7
 }
 
 /* Görev 3 */
@@ -84,7 +97,17 @@ OYUNUN KURALLARI: Makas Kağıdı yener| Kağıt Taşı yener | Taş Makas'ı ye
 */
 
 function oyun(oyuncu, bilgisayar) {
-  /*buraya kodunu yazabilirsin*/
+  if (oyuncu === bilgisayar) {
+    return 'Beraberlik';
+  } else if (
+    (oyuncu === 'Taş' && bilgisayar === 'Makas') ||
+    (oyuncu === 'Makas' && bilgisayar === 'Kağıt') ||
+    (oyuncu === 'Kağıt' && bilgisayar === 'Taş')
+  ) {
+    return 'Kazandın!';
+  } else {
+    return 'Kaybettin!';
+  }
 }
 
 // Şimdi Taş, Kağıt, Makas oyununu bilgisayara karşı oynayalım!
@@ -113,8 +136,8 @@ Aşağdaki milDonusturucu fonksiyonunu aşağıdakileri kullanarak tamamla:
 3. Mil değerini geri dönün
 */
 
-function milDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function milDonusturucu(km) {
+return km * 0.621371
 }
 
 //Görev 4b - Santimetreden Feet
@@ -127,8 +150,8 @@ Aşağıdakileri feetDonusturucu fonsiyonunu kullanarak yap:
 Google'da arama ipucu: "feet cm dönüştürme"
 */
 
-function feetDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function feetDonusturucu(cm) {
+return cm / 30.48
 }
 
 /* Görev 5 : 5 küçük maymun yatakta zıplamış şarkısını çocuklar için hazırladığımı varsayalım. https://www.youtube.com/watch?v=e4EJ34xnlxk */
@@ -144,7 +167,10 @@ Aşağıdakileri cocukSarkisi fonksiyonunda yap:
 4. Bu döngüde, her seferinde cocukSarkisi fonsiyonu çalışsın ve console.log'a dönen metni yazdırsın.
 */
 
-function cocukSarkisi(/*buraya kodunu yazabilirsin*/) {
+function cocukSarkisi(sayi) {
+  for(let i = 0 ; i < 5 ; i--){
+    return `${sayi} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!`
+  }
   /*buraya kodunu yazabilirsin*/
 }
 
@@ -163,8 +189,18 @@ Aşağdakileri notHesapla fonksiyonunda yap.
  dönün
 */
 
-function notHesapla(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function notHesapla(not) {
+  if(not >= 90 && not <= 100){
+    return 'A aldın'
+  }else if(not >= 80 && not <= 89){
+    return 'B aldın'
+}else if(not >= 70 && not <= 79){
+  return 'C aldın'
+}else if(not >= 60 && not <= 69){
+  return 'D aldın'
+}else{
+  return 'F aldın'
+}
 }
 
 /* Bonus Çalışma: Sesli harf sayacı - Kaç tane sesli harf var? */
@@ -178,8 +214,17 @@ Aşağıdakileri sesliHarfSayaci fonskiyonunda yap.
 İPUCU - .includes() methoduna bakabilirsin. (https://www.w3schools.com/jsref/jsref_includes.asp)
 */
 
-function sesliHarfSayaci(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function sesliHarfSayaci(metin) {
+  const sesliHarfler = ['a', 'e', 'i', 'ı', 'o', 'ö', 'u', 'ü', 'A', 'E', 'İ', 'I', 'O', 'Ö', 'U', 'Ü'];
+  let sayac = 0;
+
+  for (let i = 0; i < metin.length; i++) {
+    if (sesliHarfler.includes(metin[i])) {
+      sayac++;
+    }
+  }
+
+  return sayac;
 }
 
 /* Lütfen bu satırın alt tarafını değiştirmeyin */
